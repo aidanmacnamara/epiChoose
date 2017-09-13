9d763042967b33deac705ba43d9ae0c1fa2cad2b
Aidan MacNamara <am673712@uk1salx00235.corpnet2.com>
2017-09-13 13:55:13 +0100

    new function

---
+++ R/data_shape.R
@@ -0,0 +1,12 @@
+#' @description TO ADD
+#' @description This is a new line ...
+#' @details What's this?
+#' @param dat
+#' @return TO ADD
+
+data_shape <- function(x) {
+  
+  return(lapply(x, function(y) dim(y[[1]])))
+  
+}
+
