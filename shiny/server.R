8501b2e1867b4240cbb322fac979ad91be89061a
Aidan MacNamara <am673712@uk1salx00235.corpnet2.com>
2017-10-09 16:09:02 +0100

    update

--- shiny/server.R
+++ shiny/server.R
@@ -1,4 +1,16 @@
-library(shiny)
+require(epiChoose)
+require(shiny)
+require(tidyverse)
+require(ggrepel)
+require(reshape2)
+require(biomaRt)
+require(rtracklayer)
+require(Sushi)
+
+load("gene_list_all.RData")
+load("t_list.RData")
+load("dat.RData")
+load("msig_go_bp.RData")
 
 shinyServer(function(input, output) {
   
@@ -12,6 +24,7 @@ shinyServer(function(input, output) {
       
       go_choice <- renderPrint({input$go_choice})()
       my_choices$go_choice = str_replace(go_choice, "^.*\"([[:alnum:]\\s]+)\"", "\\1")
+      # print(my_choices$go_choice)
       
       cell_target_choice <- renderPrint({input$cell_target_choice})()
       my_choices$cell_target_choice = unlist(regmatches(cell_target_choice, gregexpr("[[:alnum:]_]+", cell_target_choice)))[-1] # remove na added
@@ -23,7 +36,7 @@ shinyServer(function(input, output) {
     }
   )
   
-  output$dist_plot <- renderPlot({
+  output$dist_plot_1 <- renderPlot({
     
     if(
       (my_choices()$gene_choice[1]!="NULL" | my_choices()$go_choice[1]!="NULL") &
@@ -32,7 +45,7 @@ shinyServer(function(input, output) {
     ) {
       
       if(my_choices()$go_choice[1]!="NULL") {
-        genes = msig_go_bp[[which(my_choices()$go_choice==names(msig_go_bp))]]
+        genes = msig_go_bp[[which(names(msig_go_bp)==my_choices()$go_choice)]]
       } else {
         genes = my_choices()$gene_choice
       }
@@ -44,8 +57,8 @@ shinyServer(function(input, output) {
       t_cells = my_choices()$cell_target_choice
       # t_cells = "NHBE_BR1_Baseline"
       
-      col_ix = which(gene_list_all$hgnc_symbol %in% genes)
-      print(col_ix)
+      col_ix = which(colnames(dat[[1]]$res) %in% genes)
+      # print(col_ix)
       
       # slice matrices if necessary
       dat_plot = dat
@@ -59,16 +72,39 @@ shinyServer(function(input, output) {
       # c_ix = c(1:14,18:21)
       t_ix = match(t_cells, rownames(dat[[1]]$res))
       
-      print(t_ix)
-      print(c_ix)
+      # print(t_ix)
+      # print(c_ix)
       
-      res = dist_mat(dat_plot, comp_ix=list(c_ix, t_ix), labels=single_labels, plot_labels=c("BEAS2B","A549","NHLF"), plot_res=TRUE, use_corr=TRUE, font_size=30, label_size=input$label.size)
+      res = dist_mat(dat_plot, comp_ix=list(c_ix, t_ix), labels=single_labels, plot_labels=c("BEAS2B","A549","NHLF"), plot_res=TRUE, use_corr=TRUE, font_size=25, label_size=input$label.size)
       
+    } else {
+      res = dist_mat(x=dat, comp_ix=list(), labels=single_labels, plot_res=TRUE, use_corr=TRUE, font_size=25, label_size=input$label.size, plot_blank=TRUE)
     }
   })
   
+  output$dist_plot_2 <- renderPlot({
+    qplot(1,1) + theme_thesis(20)
+  })
+  
   output$plot1 <- renderPlot({
-    ggplot(dat_out, aes_string(names(dat_out)[2], names(dat_out)[3])) + geom_point(alpha=0.2) + theme_thesis()
+    
+    dat_out = spotfire_view(dat)
+    
+    if(my_choices()$gene_choice[1]!="NULL" | my_choices()$go_choice[1]!="NULL") {
+      if(my_choices()$go_choice[1]!="NULL") {
+        genes = msig_go_bp[[which(names(msig_go_bp)==my_choices()$go_choice)]]
+      } else {
+        genes = my_choices()$gene_choice
+      }
+      dat_out$color = ifelse(dat_out$Gene %in% genes, 2, 1)
+    } else {
+      dat_out$color = 1
+    }
+    
+    dat_out$color = factor(dat_out$color)
+    dat_out = tbl_df(dat_out)
+    ggplot(dat_out, aes_string(names(dat_out)[2], names(dat_out)[3])) + geom_point(size=2, aes(color=color, alpha=color)) + theme_thesis() + scale_color_discrete(guide=FALSE) + scale_alpha_manual(guide=FALSE, values=c(0.3,0.8))
+    
   })
   
   output$click_info <- renderPrint({
@@ -83,6 +119,67 @@ shinyServer(function(input, output) {
   
   output$download_table <- downloadHandler("selected_genes.csv", content=function(file) {
     write.csv(out$brush_info, file, quote=TRUE, row.names=FALSE)
+  })
+  
+  output$sushi<- renderPlot({
+    
+    sample_ix = c(1,3,15)
+    data_type = 1
+    win = 20000
+    
+    if(
+      (my_choices()$gene_choice[1]!="NULL" | my_choices()$go_choice[1]!="NULL") &
+      my_choices()$cell_target_choice[1]!="NULL" &
+      my_choices()$cell_candidate_choice[1]!="NULL"
+    ) {
+      
+      if(my_choices()$go_choice[1]!="NULL") {
+        print(my_choices()$go_choice)
+        genes = msig_go_bp[[which(names(msig_go_bp)==my_choices()$go_choice)]]
+      } else {
+        genes = my_choices()$gene_choice
+      }
+      
+      col_ix = which(colnames(dat[[1]]$res) %in% genes)
+      
+      roi = gene_list_all[col_ix]
+      start(roi) = start(roi) - win
+      end(roi) = end(roi) + win
+      
+      my_tracks = sapply(str_replace(dat[[data_type]]$annot$Bigwig[sample_ix], "/GWD/bioinfo/projects/", "z:/links/"), function(x) import.bw(x, which=roi))
+      
+      mart_1 = useMart("ensembl", dataset="hsapiens_gene_ensembl")
+      t_list = getBM(attributes=c("chromosome_name","exon_chrom_start","exon_chrom_end","ensembl_transcript_id","strand","ensembl_gene_id"), filters='hgnc_symbol', values=roi$hgnc_symbol, mart=mart_1)
+      t_list$type = "exon"
+      t_list$chromosome_name = paste0("chr", t_list$chromosome_name)
+      
+      my_tracks_df = lapply(my_tracks, function(x) as.data.frame(x)[,c(1:3,6)])
+      
+      par(mfcol=c(length(sample_ix)+1,length(roi)), mar=c(4,4,2,2))
+      
+      for(g_ix in 1:length(roi)) {
+        
+        chrom = as.character(seqnames(roi)[g_ix])
+        chromstart = start(roi)[g_ix]
+        chromend = end(roi)[g_ix]
+        
+        for(i in 1:length(sample_ix)) {
+          
+          if(g_ix==1) {
+            plotBedgraph(my_tracks_df[[i]], chrom, chromstart, chromend, transparency=.2, color=SushiColors(2)(length(sample_ix))[i], main=rownames(dat[[1]]$res)[sample_ix[i]])
+          } else{
+            plotBedgraph(my_tracks_df[[i]], chrom, chromstart, chromend, transparency=.2, color=SushiColors(2)(length(sample_ix))[i])
+          }
+          labelgenome(chrom, chromstart, chromend, n=10, scale="Mb")
+          axis(side=2, las=2, tcl=.2)
+          
+        }
+        
+        plotGenes(t_list, chrom, chromstart, chromend, types=t_list$type, labeltext=TRUE, maxrows=50, height=0.4, plotgenetype="box")
+        labelplot(title=roi$hgnc_symbol[g_ix])
+        
+      }
+    }
   })
   
 })
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,12 +1,12 @@
-library(shiny)
+require(shiny)
+require(stringr)
 
-# load("dat.RData")
+load("dat.RData")
+load("msig_go_bp.RData")
 
-# load_all("../")
 cells = rownames(dat[[1]]$res)
 genes = colnames(dat[[1]]$res)
-names(msig_go_bp) = str_replace(str_replace_all(names(msig_go_bp), "_", " "), "GO\\s+", "")
-
+# names(msig_go_bp) = str_replace(str_replace_all(names(msig_go_bp), "_", " "), "GO\\s+", "")
 
 ui <- fluidPage(
   
@@ -57,21 +57,87 @@ ui <- fluidPage(
         "Choose the GO biological process over which to make the comparison"
       ),
       
+      h3(""),
+      h3(""),
+      
+      selectInput("data_type_choice", label="Data Type Choice", 
+                  choices = as.list(sort(names(dat))),
+                  multiple=FALSE),
+      
+      helpText(
+        "Choose which data type to use"
+      ),
+      
       width=3
     ),
     
     mainPanel(
       tabsetPanel(
+        
         id = "tabs",
+        
         tabPanel("Gene Choice",
-                 plotOutput("dist_plot", height=800),
-                 sliderInput("label.size", NULL, min=1, max=10, value=4, step=1)
+                 
+                 h3(""),
+                 h3(""),
+                 
+                 column(width=6,
+                        plotOutput("dist_plot_1", height=600),
+                        sliderInput("label.size", NULL, min=1, max=10, value=4, step=1)
+                 ),
+                 column(width=6,
+                        plotOutput("dist_plot_2", height=600)
+                 )
         ),
+        
         tabPanel("Model Choice",
                  column(width=6,
                         plotOutput("plot1", height=600,
                                    click = "plot1_click",
                                    brush = brushOpts(id="plot1_brush")
+                        ),
+                        
+                        h3(""),
+                        h3(""),
+                        
+                        column(width=4,     
+                               selectInput("candidate_choice", label="Candidate Choice", 
+                                           choices = as.list(LETTERS[1:3]),
+                                           multiple=TRUE),
+                               
+                               helpText(
+                                 "Choose the model candidate"
+                               )
+                        ),
+                        
+                        column(width=4,     
+                               selectInput("alt_choice", label="Alternative Choice", 
+                                           choices = as.list(LETTERS[4:6]),
+                                           multiple=TRUE),
+                               
+                               helpText(
+                                 "Choose the model alternative"
+                               )
+                        ),
+                        
+                        column(width=2,     
+                               selectInput("x_axis", label="X-axis", 
+                                           choices = as.list(LETTERS[4:6]),
+                                           multiple=TRUE),
+                               
+                               helpText(
+                                 "Choose the data type for the x-axis"
+                               )
+                        ),
+                        
+                        column(width=2,     
+                               selectInput("y_axis", label="Y-axis", 
+                                           choices = as.list(LETTERS[4:6]),
+                                           multiple=TRUE),
+                               
+                               helpText(
+                                 "Choose the data type for the y-axis"
+                               )
                         )
                  ),
                  column(width=6,
@@ -82,9 +148,15 @@ ui <- fluidPage(
                    'downloadData',
                    'Download'
                  )
+        ),
+        
+        tabPanel("Gene Browser",
+                 plotOutput("sushi", height=800)
         )
       ),
+      
       width=9
+      
     )
   )
 )
