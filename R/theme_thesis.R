0425f3834d24b5f7ae166bfc2f897118e7760a30
Aidan MacNamara <am673712@uk1salx00235.corpnet2.com>
2017-11-10 10:27:24 +0000

    update

---
+++ R/theme_thesis.R
@@ -0,0 +1,14 @@
+#' @title Plotting parameters
+#' @description TO ADD
+#' @description This is a new line ...
+#' @details What's this?
+#' @return TO ADD
+#' @export
+
+theme_thesis <- function(base_size=30) {
+  
+  theme_bw(base_size=base_size) +
+    theme(text=element_text(size=base_size, family = ""), axis.title=element_text(size=rel(1)), axis.text=element_text(size = rel(0.75)), axis.title.y=element_text(vjust = 0.3), axis.title.x=element_text(vjust=0.3), plot.title=element_text(size=rel(1.33), vjust = 2), legend.title=element_blank(), legend.key=element_blank())
+  
+}
+
