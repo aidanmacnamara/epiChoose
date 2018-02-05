a42d8693984f39e3b0f430d31d8f31ba7801c285
aidanmacnamara <aidan.macnamara@gmail.com>
2018-02-05 13:11:21 +0000

    update

--- test/test_distance_project_2.R
+++ test/test_distance_project_2.R
@@ -16,6 +16,105 @@ lapply(bamfiles, seqinfo)
 register(MulticoreParam())
 se <- summarizeOverlaps(features=roi_reg, reads=bamfiles, mode="Union", ignore.strand=TRUE)
 
+col_data = data_gsk[match(colnames(se), str_extract(data_gsk$Bam, "[[:alnum:]\\._-]+$")),] %>% select(Cell, Mark, Rep, Stimulus)
+col_data = data.frame(lapply(col_data, factor))
+
+rownames(col_data) = rownames(colData(se))
+colData(se) <- DataFrame(col_data)
+
+# slice se to relevant conditions
+se_filt = se[,-grep("VD3", colData(se)$Stimulus)]
+
+# some pca
+
+# pma vs. baseline, lps vs. baseline
+
+dds <- DESeqDataSet(se_filt, design=~Mark+Rep+Stimulus)
+nrow(dds)
+dds <- dds[rowSums(counts(dds))>1,]
+nrow(dds)
+dds <- estimateSizeFactors(dds)
+
+rld = rlog(dds, blind=FALSE)
+
+
+
+
+
+# RNA RESPONSE ------------------------------------------------------------
+
+thp_diff = read_excel("z:/links/bix-analysis-stv/2016/CTTV/THP1/documents/AllGenes_THP1_U937_Altius_DE.xls")
+thp_diff_filt = filter(thp_diff, THP1_LPS_padj<0.01, THP1_LPS_log2FoldChange>2) %>% arrange(desc(THP1_LPS_log2FoldChange))
+
+
+# PLOTS -------------------------------------------------------------------
+
+n_peaks = unlist(lapply(peak_list, length))
+col_data = data.frame(col_data, peaks=n_peaks[sapply(paste(col_data$donor, col_data$time, sep="_"), function(x) grep(x, names(peak_list)))])
+ggplot(col_data, aes(x=time, y=peaks)) + geom_boxplot() + theme_thesis()
+
+rld <- rlog(dds, blind=FALSE)
+head(assay(rld), 3)
+
+par(mfrow=c(1,2))
+plot(log2(counts(dds, normalized=TRUE)[,1:2]+1), pch=16, cex=0.3)
+plot(assay(rld)[,1:2], pch=16, cex=0.3)
+
+sampleDists <- dist(t(assay(rld)))
+sampleDistMatrix <- as.matrix(sampleDists)
+rownames(sampleDistMatrix) <- rld$cell
+# colnames(sampleDistMatrix) <- NULL
+colors <- colorRampPalette(rev(brewer.pal(9, "Blues")))(255)
+pheatmap(sampleDistMatrix, clustering_distance_rows=sampleDists, clustering_distance_cols=sampleDists, col=colors)
+plotPCA(rld, intgroup=c("time"))
+
+
+# DESEQ -------------------------------------------------------------------
+
+dds <- DESeq(dds)
+res_all = list()
+res_all[[1]] <- results(dds, contrast=c("time","3hr","Un"), alpha=0.05, lfcThreshold=1.5)
+res_all[[2]] <- results(dds, contrast=c("time","24hr","Un"), alpha=0.05, lfcThreshold=1.5)
+res_all[[3]] <- results(dds, contrast=c("time","48hr","Un"), alpha=0.05, lfcThreshold=1.5)
+
+
+# ANNOTATE ----------------------------------------------------------------
+
+annot_c = annotatePeak(consensus_reduce, TxDb=TxDb.Hsapiens.UCSC.hg38.knownGene, annoDb="org.Hs.eg.db")
+peak_anno_all = list()
+annot_filts = list()
+
+for(i in 1:length(res_all)) {
+  
+  res = res_all[[i]]
+  summary(res)
+  
+  res = tbl_df(cbind(as.data.frame(annot_c), res))
+  p_ix = which(res$padj<=0.05 & res$log2FoldChange>0)
+  res_filt = tbl_df(as.data.frame(res)[p_ix,])
+  res_filt = res_filt[order(res_filt$log2FoldChange, decreasing=TRUE),]
+  
+  annot_filt = annotatePeak(consensus_reduce[p_ix], TxDb=TxDb.Hsapiens.UCSC.hg38.knownGene, annoDb="org.Hs.eg.db")
+  print(upsetplot(annot_filt))
+  
+  annot_filts[[i]] = annot_filt
+  peak_anno_all[[i]] = res_filt
+  
+}
+
+venn_func <- function(x) {
+  y = paste(as.character(unlist(x[1:3])), collapse="_")
+  return(y)
+}
+
+require(VennDiagram)
+venn.diagram(list(
+  hr_3 = apply(peak_anno_all[[1]], 1, venn_func),
+  hr_24 = apply(peak_anno_all[[2]], 1, venn_func),
+  hr_48 = apply(peak_anno_all[[3]], 1, venn_func)
+), filename = "out.tiff")
+
+
 
 # ALL DATA SUMMARY --------------------------------------------------------
 
