b52fe2b617546f26453f255ed1de8bf5b9110d4e
Aidan MacNamara <am673712@GSKWMC02X788DJG5H.local>
2018-12-19 14:26:06 +0000

    update

--- test/test_figure_3_include_enhancers.R
+++ test/test_figure_3_include_enhancers.R
@@ -6,14 +6,15 @@ load("data/dat_all.RData")
 load("data/roi_reg.RData")
 load("data/gene_list_all.RData")
 
+enhancer_ix = which(roi_reg$feature_type_name=="Enhancer" & roi_reg$SYMBOL %in% gene_list_all$hgnc_symbol & roi_reg$distanceToTSS<1e3)
+roi_reg_enhancer = roi_reg[enhancer_ix]
 
-# LOOK AT BLUEPRINT DATA ALONE: H3K27AC -----------------------------------
 
-# PICK SAMPLES ------------------------------------------------------------
+# DDS SANQUIN -------------------------------------------------------------
 
-row_ix = which(grepl("sanquin", rownames(dat_all$tss$H3K27ac$res), ignore.case=TRUE) & !is.na(dat_all$tss$H3K27ac$annot$Name))
+row_ix = which(grepl("sanquin", rownames(total_reg$H3K27ac$res), ignore.case=TRUE) & !is.na(total_reg$H3K27ac$annot$Name))
 
-col_data = data.frame(label=rownames(dat_all$tss$H3K27ac$res)[row_ix])
+col_data = data.frame(label=rownames(total_reg$H3K27ac$res)[row_ix])
 my_time = str_extract_all(col_data$label, "[0-9]+(hr[s]*|days)")
 col_data$time = NA
 col_data$challenge = 0
@@ -38,11 +39,7 @@ c_ix = which(col_data$challenge==1)
 row_ix_filt = row_ix[-c_ix]
 col_data_filt = col_data[-c_ix,]
 
-
-# SLICE AND RUN DDS/RLOG --------------------------------------------------
-
-dat_add = dat_all$tss$H3K27ac$res[row_ix_filt,]
-# dat_add = total_reg$H3K27ac$res[row_ix_filt,which(roi_reg$feature_type_name=="Enhancer")]
+dat_add = total_reg$H3K27ac$res[row_ix_filt,enhancer_ix]
 dat_add[is.na(dat_add)] = 0
 dat_add = apply(dat_add, 2, as.integer)
 dim(dat_add)
@@ -50,240 +47,22 @@ dds_sanquin = DESeqDataSetFromMatrix(countData=t(dat_add), colData=col_data_filt
 rld_sanquin = vst(dds_sanquin, blind=FALSE)
 
 
-# PLOT HEATMAP/PCA --------------------------------------------------------
+# DDS CELL LINES ----------------------------------------------------------
 
-sampleDists <- dist(t(assay(rld_sanquin)))
-sampleDistMatrix <- as.matrix(sampleDists)
-rownames(sampleDistMatrix) = paste(rld_sanquin$treatment, rld_sanquin$time, sep="_")
-colors <- colorRampPalette(rev(brewer.pal(9, "Blues")))(255)
+row_ix = which(grepl("thp-1|u937", rownames(total_reg$H3K27ac$res), ignore.case=TRUE))
 
-png(filename="c:/Users/am673712/Dropbox/OTAR020/images/sanquin_image_1.png", width=1000, height=600)
-pheatmap(sampleDistMatrix, clustering_distance_rows=sampleDists, clustering_distance_cols=sampleDists, col=colors, show_colnames=FALSE)
-dev.off()
-
-png(filename="c:/Users/am673712/Dropbox/OTAR020/images/sanquin_image_2.png", width=1000, height=600)
-pca_and_plot(rld_sanquin, annot_1=paste(rld_sanquin$treatment, rld_sanquin$time, sep="_"), annot_2=rld_sanquin$donor)
-dev.off()
-
-
-# TOP N HITS --------------------------------------------------------------
-
-# novakovic criteria
-# fc 3
-# adj p 0.05
-# rpkm > 1
-
-design(dds_sanquin)
-dds_sanquin = DESeq(dds_sanquin, test="LRT", reduced=~donor)
-res_sanquin = tbl_df(results(dds_sanquin))
-res_sanquin$gene = rownames(res_sanquin)
-res_sanquin$symbol = gene_list_all$hgnc_symbol
-arrange(res_sanquin, padj) %>% filter(padj<1e-8)
-
-# take the top 1000 hits
-genes_top = head(res_sanquin$gene[order(res_sanquin$padj)],1e3)
-
-for(j in 1:3) {
-  y = plotCounts(dds_sanquin, gene=head(order(res_sanquin$padj),10)[j], intgroup=c("treatment","time","donor"), returnData=TRUE)
-  print(ggplot(y, aes(x=time, y=count)) + geom_point(shape=17, size=3) + theme_thesis(20) + xlab("") + ylab("Count") + ggtitle(res_sanquin$gene[head(order(res_sanquin$padj),10)[j]]))
-}
-
-y = t(assays(rld_sanquin)[[1]])
-dim(y)
-y = y[,colnames(y) %in% genes_top]
-y = y[,!apply(y, 2, function(x) sd(x)==0)] # remove regions with no variance
-dim(y)
-
-pca_res <- prcomp(y, scale=TRUE, center=TRUE)
-pca_res_summary = summary(pca_res)
-yy = data.frame(pca_res$x[,1:2])
-names(yy) = c("x","y")
-yy$annot_1 = paste(rld_sanquin$treatment, rld_sanquin$time, sep="_")
-yy$annot_2 = rld_sanquin$donor
-ggplot(yy, aes(x=x, y=y, color=annot_2)) + geom_point(size=5, shape=17) + xlab(paste0("PC", 1, ": ", pca_res_summary$importance[2,1]*100, "%")) + ylab(paste0("PC", 2, ": ", pca_res_summary$importance[2,2]*100, "%")) + theme_thesis() + geom_text_repel(aes(label=annot_1), fontface="bold", size=5, force=0.5) + theme(legend.position="none")
-
-for_heatmap = assays(rld_sanquin)[[1]]
-colnames(for_heatmap) = paste(col_data_filt$treatment, col_data_filt$time, col_data_filt$donor, sep="_")
-for_heatmap = as.data.frame(for_heatmap[match(genes_top,rownames(dds_sanquin)),])
-dim(for_heatmap)
-pheatmap(for_heatmap, show_rownames=FALSE)
-
-wss = (nrow(for_heatmap)-1) * sum(apply(for_heatmap,2,var))
-for(k in 2:15) {
-  wss[k] <- sum(kmeans(for_heatmap, centers=k)$withinss)
-}
-plot(1:15, wss, type="b", xlab="Number of Clusters", ylab="Within-group sum-of-squares")
-fit <- kmeans(for_heatmap, 6)
-
-pheatmap(for_heatmap, fontsize_row=8,
-         annotation_row = data.frame(
-           K_Means = factor(fit$cluster),
-           row.names=rownames(for_heatmap)
-         ), show_rownames=FALSE
-)
-
-for_heatmap$Gene = rownames(for_heatmap)
-for_heatmap$Cluster = factor(fit$cluster)
-head(for_heatmap)
-for_heatmap = gather(for_heatmap, "Group", "AUC", 1:17)
-for_heatmap$Group = factor(for_heatmap$Group)
-ggplot(for_heatmap, aes(x=Group,y=AUC,group=Gene)) + geom_point(shape=17) + geom_line(alpha=0.1) + facet_wrap(~Cluster) + theme_thesis(15)
-
-
-# MONOCYTE/MACROPHAGE RELEVANT GENES --------------------------------------
-
-# pick the gene set from saeed/novakovic
-p_genes = read_excel("tmp/1-s2.0-S0092867416313162-mmc2.xlsx", sheet="Table S2B. Genes H3K27ac prom", skip=1)
-dg_genes = p_genes$Promoter_H3K27ac_Differentiation_gain; dg_genes = dg_genes[!is.na(dg_genes)]
-dl_genes = p_genes$Promoter_H3K27ac_Differentiation_loss; dl_genes = dl_genes[!is.na(dl_genes)]
-
-for_heatmap = assays(rld_sanquin)[[1]]
-colnames(for_heatmap) = paste(col_data_filt$treatment, col_data_filt$time, col_data_filt$donor, sep="_")
-match_ix = match(c(dg_genes,dl_genes), gene_list_all$hgnc_symbol); match_ix = match_ix[!is.na(match_ix)]
-for_heatmap = as.data.frame(for_heatmap[match_ix,])
-dim(for_heatmap)
-
-pheatmap(for_heatmap, cluster_rows=FALSE, show_rownames=FALSE)
-col_clust = hclust(dist(t(for_heatmap)))
-col_names = paste0("Group_", cutree(col_clust, k=2))
-names(for_heatmap) = paste(names(for_heatmap), col_names, sep="_")
-head(for_heatmap)
-for_heatmap$Gene = gene_list_all$hgnc_symbol[match_ix]
-for_heatmap$Cluster = factor(ifelse(for_heatmap$Gene %in% dg_genes, "Up", "Down"))
-head(for_heatmap)
-
-for_heatmap_long = gather(for_heatmap,"Group","AUC",1:24)
-for_heatmap_long$Group = factor(for_heatmap_long$Group)
-head(for_heatmap_long)
-for_heatmap_long$all_group = str_extract(for_heatmap_long$Group, "Group_[12]")
-ggplot(for_heatmap_long, aes(x=all_group,y=AUC)) + geom_boxplot() + facet_wrap(~Cluster) + theme_thesis(20)
-
-
-# BRING IN THP-1/U937 -----------------------------------------------------
-
-# PICK SAMPLES ------------------------------------------------------------
-
-row_ix = which(grepl("thp-1|u937", rownames(dat_all$tss$H3K27ac$res), ignore.case=TRUE))
-
-col_data = dat_all$tss$H3K27ac$annot[row_ix,]
+col_data = total_reg$H3K27ac$annot[row_ix,]
 col_data_filt = data.frame(dplyr::select(col_data, Label))
 col_data_filt$rep = str_extract(col_data_filt$Label, "BR[12]")
 col_data_filt$condition = str_extract(col_data_filt$Label, "[[:alnum:]+]+$")
 col_data_filt$cell_line = str_extract(col_data_filt$Label, "^[[:alnum:]-]+")
 
-
-# RUN DDS -----------------------------------------------------------------
-
-dat_add = dat_all$tss$H3K27ac$res[row_ix,]
-# dat_add = total_reg$H3K27ac$res[row_ix,which(roi_reg$feature_type_name=="Enhancer")]
+dat_add = total_reg$H3K27ac$res[row_ix,enhancer_ix]
 dat_add[is.na(dat_add)] = 0
 dat_add = apply(dat_add, 2, as.integer)
 dim(dat_add)
 dds_cell_lines = DESeqDataSetFromMatrix(countData=t(dat_add), colData=col_data_filt, design=~rep+condition+cell_line)
 rld_cell_lines = vst(dds_cell_lines, blind=FALSE)
-
-
-# PLOT RESULTS ------------------------------------------------------------
-
-sampleDists <- dist(t(assay(rld_cell_lines)))
-sampleDistMatrix <- as.matrix(sampleDists)
-rownames(sampleDistMatrix) = paste(rld_cell_lines$condition, rld_cell_lines$rep, sep="_")
-colors <- colorRampPalette(rev(brewer.pal(9, "Blues")))(255)
-
-pheatmap(sampleDistMatrix, clustering_distance_rows=sampleDists, clustering_distance_cols=sampleDists, col=colors, show_colnames=FALSE)
-
-pca_and_plot(rld_cell_lines, annot_1=rld_cell_lines$condition, annot_2=rld_cell_lines$cell_line)
-
-
-# DIFFERENTIAL PMA VS. BASELINE -------------------------------------------
-
-design(dds_cell_lines)
-dds_cell_lines = DESeq(dds_cell_lines)
-res_cell_lines = tbl_df(results(dds_cell_lines, contrast=c("condition","PMA","Baseline")))
-res_cell_lines$gene = rownames(res_cell_lines)
-res_cell_lines$symbol = gene_list_all$hgnc_symbol
-arrange(res_cell_lines, padj)
-
-
-# COMPARE THP1/U937 AND SANQUIN TOGETHER ----------------------------------
-
-# PICK SAMPLES ------------------------------------------------------------
-
-row_ix = which(grepl("thp-1|u937", rownames(dat_all$tss$H3K27ac$res), ignore.case=TRUE) | grepl("sanquin", rownames(dat_all$tss$H3K27ac$res), ignore.case=TRUE) & !is.na(dat_all$tss$H3K27ac$annot$Name))
-row_ix = c(row_ix, which(rownames(dat_all$tss$H3K27ac$res)=="Monocytes-CD14+_Broad"))
-
-col_data = data.frame(label=rownames(dat_all$tss$H3K27ac$res)[row_ix])
-my_time = str_extract_all(col_data$label, "[0-9]+(hr[s]*|days)")
-col_data$time = NA
-col_data$challenge = 0
-for(i in 1:length(my_time)) {
-  if(grepl("LPS_T=4hrs$", col_data$label[i]) & length(my_time[[i]])>1) {
-    col_data$challenge[i] = 1
-    col_data$time[i] = my_time[[i]][2]
-  } else if(length(my_time[[i]])==2) {
-    col_data$time[i] = my_time[[i]][2]
-  } else {
-    col_data$time[i] = my_time[[i]][1]
-  }
-}
-col_data$treatment = str_replace(col_data$label, "^.*?([[:alpha:]_]+)_T.*$", "\\1")
-col_data$treatment = str_replace(col_data$treatment, "RPMI_", "")
-col_data$donor = str_replace(col_data$label, "^.*mono_([0-9]+).*$", "\\1")
-col_data$treatment[col_data$treatment=="RPMI"] = "Naive"
-rownames(col_data) = rownames(dat_all$tss$H3K27ac$res)[row_ix]
-col_data[length(row_ix),2:5] = c("NA",0,"Naive","Broad")
-
-# remove challenge data
-c_ix = which(col_data$challenge==1)
-row_ix_filt = row_ix[-c_ix]
-col_data_filt = col_data[-c_ix,]
-col_data_filt$treatment[25:48] = str_extract(col_data_filt$label[25:48], "[[:alnum:]+]+$")
-col_data_filt$time[25:48] = "0days"
-col_data_filt$donor[25:48] = "None"
-col_data_filt$cell_type = str_extract(col_data_filt$label, "monocyte|THP-1|U937")
-col_data_filt$cell_type[dim(col_data_filt)[1]] = "monocyte"
-
-dat_add = dat_all$tss$H3K27ac$res[row_ix_filt,]
-# dat_add = total_reg$H3K27ac$res[row_ix_filt,which(roi_reg$feature_type_name=="Enhancer")]
-dat_add[is.na(dat_add)] = 0
-dat_add = apply(dat_add, 2, as.integer)
-dim(dat_add)
-dds_all = DESeqDataSetFromMatrix(countData=t(dat_add), colData=col_data_filt, design=~treatment+time)
-rld_all = vst(dds_all, blind=FALSE)
-
-
-# PLOT HEATMAP/PCA --------------------------------------------------------
-
-sampleDists <- dist(t(assay(rld_all)))
-sampleDistMatrix <- as.matrix(sampleDists)
-rownames(sampleDistMatrix) = paste(rld_all$cell_type, rld_all$treatment, rld_all$time, sep="_")
-colors <- colorRampPalette(rev(brewer.pal(9, "Blues")))(255)
-
-pheatmap(sampleDistMatrix, clustering_distance_rows=sampleDists, clustering_distance_cols=sampleDists, col=colors, show_colnames=FALSE)
-
-pca_and_plot(rld_all, annot_1=paste(rld_all$cell_type, rld_all$treatment, rld_all$time, sep="_"), annot_2=factor(c(rep(1,48),2)))
-
-
-# WHAT IS DRIVING PC1 DIFFERENCES? ----------------------------------------
-
-y = t(assays(rld_all)[[1]])
-dim(y)
-pca_res <- prcomp(y, scale=TRUE, center=TRUE)
-
-dat = data.frame(gene=gene_list_all$hgnc_symbol, loadings=pca_res$rotation[,1])
-ranks = deframe(dat)
-p = gmtPathways("tmp/c2.cp.v6.2.symbols.gmt")
-
-res <- fgsea(pathways=p, stats=ranks, nperm=1000)
-res_tidy = res %>% as_tibble() %>% arrange(desc(NES))
-res_tidy %>% dplyr::select(-leadingEdge, -ES, -nMoreExtreme) %>% arrange(padj) %>% DT::datatable()
-
-print(ggplot(filter(res_tidy, padj<0.03), aes(reorder(pathway, NES), NES)) + geom_col(aes(fill=padj<0.03)) + coord_flip() + labs(x="Pathway", y="Normalized Enrichment Score") + theme_thesis(10))
-
-# remove the first pc and project again?
-y_rev = pca_res$x[,-1] %*% t(pca_res$rotation[,-1])
-dim(y_rev)
-pca_and_plot(y_rev, annot_1=paste(rld_all$cell_type, rld_all$treatment, rld_all$time, sep="_"), annot_2=NA) # still orthogonal, easier to look gene-by-gene
 
 
 # FC TABLE ----------------------------------------------------------------
@@ -296,7 +75,7 @@ design(dds_sanquin) = formula(~time_treatment+donor)
 dds_sanquin = DESeq(dds_sanquin)
 
 trts_sanquin = as.character(unique(dds_sanquin$time_treatment)[-1])
-res_sanquin = data.frame(genes=gene_list_all$hgnc_symbol)
+res_sanquin = data.frame(loci=colnames(dat_add))
 for(i in 1:length(trts_sanquin)) {
   res = results(dds_sanquin, contrast=c("time_treatment",trts_sanquin[i],"1hr_Attached"))
   res_sanquin = cbind(res_sanquin, res$log2FoldChange, res$padj)
@@ -316,7 +95,7 @@ design(dds_cell_lines)
 dds_cell_lines = DESeq(dds_cell_lines)
 trts_thp1 = as.character(unique(dds_cell_lines$cell_condition)[2:6]) # pick out the treatment conditions
 trts_u937 = as.character(unique(dds_cell_lines$cell_condition)[8:12])
-res_cell_lines = data.frame(genes=gene_list_all$hgnc_symbol)
+res_cell_lines = data.frame(loci=colnames(dat_add))
 
 # add contrasts across thp-1 and u937
 for(i in 1:length(trts_thp1)) {
@@ -338,8 +117,8 @@ res_all = rbind(res_cell_lines, res_sanquin) # fc and p values
 # split fc and p values to 2 data frames
 res_all_p = filter(res_all, type=="p") %>% dplyr::select(-type) %>% spread(group, score)
 res_all_fc = filter(res_all, type=="fc") %>% dplyr::select(-type) %>% spread(group, score)
-rownames(res_all_p) = res_all_p$genes
-rownames(res_all_fc) = res_all_fc$genes
+rownames(res_all_p) = res_all_p$loci
+rownames(res_all_fc) = res_all_fc$loci
 res_all_p = res_all_p[,-1]
 res_all_fc = res_all_fc[,-1]
 all(names(res_all_p)==names(res_all_fc))
